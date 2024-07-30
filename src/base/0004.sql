2d7eb0a15caa87723214d01006a2fddb2c20fad5
itallonet <italloriancs@gmail.com>
2024-07-29 21:47:45 -0300

    feature: base auth2

---
+++ src/base/0004.sql
@@ -0,0 +1,8 @@
+CREATE TABLE entity_roles (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMPTZ DEFAULT now(),
+    updated_at TIMESTAMPTZ DEFAULT NULL
+    entity_id UUID REFERENCES entity(id) ON DELETE CASCADE,
+    role_id UUID REFERENCES roles(id) ON DELETE CASCADE,
+);
